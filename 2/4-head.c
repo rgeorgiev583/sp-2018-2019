4a9b9866b81943d8210594a3624e6d26906b8341
Radoslav Georgiev <rgeorgiev583@gmail.com>
2018-10-15 00:56:01 +0300

    fix some semantic errors in the implementation of the solution to the fourth problem from the second meeting

--- 2/4-head.c
+++ 2/4-head.c
@@ -6,11 +6,11 @@ int main(int argc, const char* const* argv)
     if (argc < 2)
         return 1;
 
-    int fd = open(argv[1], O_RDONLY, 0644);
+    int fd = open(argv[1], O_RDONLY);
     int total_count = atoi(argv[2]);
     char buffer;
     size_t current_count = 0;
-    while (current_count < total_count && (current_count = read(fd, buffer, 1)))
-        write(1, buffer, 1);
+    while (current_count < total_count && read(fd, &buffer, 1) > 0)
+        write(1, &buffer, 1);
     return 0;
 }

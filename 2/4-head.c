69994cd4d9eacda4df246bd0232b0403df3d003e
Radoslav Georgiev <rgeorgiev583@gmail.com>
2018-10-15 00:08:13 +0300

    fix some build errors in the implementation of the solution to the fourth problem from the second meeting

--- 2/4-head.c
+++ 2/4-head.c
@@ -8,9 +8,9 @@ int main(int argc, const char* const* argv)
 
     int fd = open(argv[1], O_RDONLY, 0644);
     int total_count = atoi(argv[2]);
-    char c;
+    char buffer;
     size_t current_count = 0;
-    while (current_count < total_count && (count = read(fd, buffer, 1)))
+    while (current_count < total_count && (current_count = read(fd, buffer, 1)))
         write(1, buffer, 1);
     return 0;
 }

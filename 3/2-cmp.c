af7b45854890e4f7e6bac28717f2665844b2040b
Radoslav Georgiev <rgeorgiev583@gmail.com>
2018-10-22 09:17:26 +0300

    fixed a bug where the `cmp` implementation wrongly reported that two identical files differed

--- 3/2-cmp.c
+++ 3/2-cmp.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define NEWLINE_CHARACTER '\n'
 
@@ -16,13 +17,24 @@ int main(int argc, const char* const* argv)
     char buffer1, buffer2;
     size_t current_byte = 0;
     size_t current_line = 0;
-    while (read(fileno1, &buffer1, 1) > 0 && read(fileno2, &buffer2, 1) > 0 && buffer1 == buffer2)
+    bool is_eof1 = false, is_eof2 = false;
+    do
     {
+        is_eof1 = read(fileno1, &buffer1, 1) <= 0;
+        is_eof2 = read(fileno2, &buffer2, 1) <= 0;
+        if (is_eof1 || is_eof2)
+            break;
+
         current_byte++;
         if (NEWLINE_CHARACTER == buffer1)
             current_line++;
     }
-    printf("%s %s differ: byte %u, line %u\n", argv[1], argv[2], current_byte + 1, current_line + 1);
+    while (buffer1 == buffer2);
+
+    if (!is_eof1 || !is_eof2)
+    {
+        printf("%s %s differ: byte %u, line %u\n", argv[1], argv[2], current_byte, current_line);
+    }
 
     return 0;
 }

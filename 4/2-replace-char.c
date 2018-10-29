f451d0ca68f1848a9ab81dd465ef4a45c9da2084
Radoslav Georgiev <rgeorgiev583@gmail.com>
2018-10-29 08:47:54 +0200

    replace-char: disallow characters of a length greater than one byte

--- 4/2-replace-char.c
+++ 4/2-replace-char.c
@@ -13,9 +13,11 @@ int main(int argc, const char* const* argv)
     if (argc < REQUIRED_ARG_COUNT + 1)
         return 1;
 
+    if (strlen(argv[3]) > 1)
+        return 1;
+
     int from_position = atoi(argv[1]);
     int to_position = atoi(argv[2]);
-    int char_length = strlen(argv[3]);
     int fileno = open(argv[4], O_WRONLY, DEFAULT_FILE_MODE);
     if (fileno < 0)
     {
@@ -25,7 +27,7 @@ int main(int argc, const char* const* argv)
 
     lseek(fileno, from_position, from_position >= 0 ? SEEK_SET : SEEK_END);
     for (size_t i = from_position; i <= to_position; i++)
-        write(fileno, argv[3], char_length);
+        write(fileno, argv[3], 1);
 
     return 0;
 }

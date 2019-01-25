673ca061ad082331b9c94e46519265b07bd9b06d
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-01-25 02:21:43 +0200

    drop redundant check for command-line argument validity

--- 4/1-replace-word.c
+++ 4/1-replace-word.c
@@ -16,8 +16,6 @@ int main(int argc, const char* const* argv)
     ssize_t position = atol(argv[1]);
 
     ssize_t word_length = strlen(argv[2]);
-    if (word_length <= 0)
-        exit(2);
 
     int fileno = open(argv[3], O_WRONLY, DEFAULT_FILE_MODE);
     if (-1 == fileno)

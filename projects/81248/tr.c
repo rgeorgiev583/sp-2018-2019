8fdd82a902ccf2e2273ddec000b85aef4845b6d8
Georgi Lyubenov <godzbanebane@gmail.com>
2019-01-26 17:29:35 +0200

    Handle complement flag

--- projects/81248/tr.c
+++ projects/81248/tr.c
@@ -24,6 +24,18 @@ void handle_flags(const char* args, bool* complement, bool* delete, bool* nub)
     }
 }
 
+char last(const char* str)
+{
+    char curr = str[0];
+
+    for(int i = 0; str[i] != 0; ++i)
+    {
+        curr = str[i];
+    }
+
+    return curr;
+}
+
 int main (int argc, const char* const * argv)
 {
     if (argc < REQUIRED_ARGS + 1)
@@ -45,32 +57,52 @@ int main (int argc, const char* const * argv)
         handle_flags(argv[1], &complement, &delete, &nub);
     }
 
-    printf("%d %d %d\n", complement, delete, nub);
-
     char mapping[256] = {0};
-
-    for(int i = 0; i < 256; ++i)
-    {
-        mapping[i] = i;
-    }
 
     const char* const inset = argv[argstart];
     const char* const outset = argv[argstart + 1];
-    int outindex = 0;
 
-    for(int i = 0; inset[i] != 0; ++i)
+    if (complement)
     {
-        mapping[(int)inset[i]] = outset[outindex];
+        char subst_char = last(outset);
 
-        if(!outset[outindex + 1] == 0)
+        // set everything to the required char
+        for(int i = 0; i < 256; ++i)
         {
-            ++outindex;
+            mapping[i] = subst_char;
+        }
+
+        // only unset those that are our in inset
+        // we get a complement this way
+        for(int i = 0; inset[i] != 0; ++i)
+        {
+            mapping[(int)inset[i]] = inset[i];
+        }
+    }
+    else
+    {
+        for(int i = 0; i < 256; ++i)
+        {
+            mapping[i] = i;
+        }
+
+        int outindex = 0;
+
+        for(int i = 0; inset[i] != 0; ++i)
+        {
+            mapping[(int)inset[i]] = outset[outindex];
+
+            // if we are at the end of the outset we need to not move
+            if(!outset[outindex + 1] == 0)
+            {
+                ++outindex;
+            }
         }
     }
 
     int curr = 0;
 
-    while((curr = getc(stdin)) != EOF)
+    while((curr = getchar()) != EOF)
     {
         putchar(mapping[curr]);
     }

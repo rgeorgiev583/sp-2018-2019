f0c2e2e159a6c686efbd15eebc8ca2eb52b00e13
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-01-30 09:42:10 +0200

    pkr: trfc: drop the `trf` function as it isn't applicable for STDIN_FILENO

--- pkr/2-2-trfc.c
+++ pkr/2-2-trfc.c
@@ -8,28 +8,14 @@
 
 #define REQUIRED_ARG_COUNT 2
 
-static const char* set1;
-static const char* set2;
-
-static void trf(int input_fileno)
-{
-    dup2(input_fileno, STDIN_FILENO);
-
-    if (-1 == execlp("tr", "tr", set1, set2, NULL))
-    {
-        perror("exec");
-        exit(8);
-    }
-}
-
 int main(int argc, char* const* argv)
 {
     if (argc < REQUIRED_ARG_COUNT + 1)
         exit(1);
 
-    set2 = argv[1];
+    const char* set2 = argv[1];
 
-    set1 = argv[2];
+    const char* set1 = argv[2];
 
     if (argc > REQUIRED_ARG_COUNT + 1)
     {
@@ -50,16 +36,27 @@ int main(int argc, char* const* argv)
                         exit(5);
                     }
 
-                    trf(input_fileno);
+                    dup2(input_fileno, STDIN_FILENO);
+                    if (-1 == execlp("tr", "tr", set1, set2, NULL))
+                    {
+                        perror("exec");
+                        exit(8);
+                    }
                 }
             }
         }
 
-        for (int i = 1; i < argc; i++)
+        for (int i = 3; i < argc; i++)
             wait(NULL);
     }
     else
-        trf(STDIN_FILENO);
+    {
+        if (-1 == execlp("tr", "tr", set1, set2, NULL))
+        {
+            perror("exec");
+            exit(8);
+        }
+    }
 
     return 0;
 }
